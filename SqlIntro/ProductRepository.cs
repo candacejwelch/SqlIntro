a276b0a8ec60bcaaa6d8b968ff5bf3281ae5c75e
Candace <candacejwelch@gmail.com>
2017-10-24 07:48:47 -0500

    Select, delete, update, and insert completed

--- SqlIntro/ProductRepository.cs
+++ SqlIntro/ProductRepository.cs
@@ -25,12 +25,18 @@ namespace SqlIntro
         {
             using (var conn = new MySqlConnection(_connectionString))
             {
+
+                conn.Open();
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = ""; //TODO:  Write a SELECT statement that gets all products
+                cmd.CommandText = "select Name, ModifiedDate from product where ModifiedDate > '2000-10-19' and '2017-10-20';"; //TODO:  Write a SELECT statement that gets all products
                 var dr = cmd.ExecuteReader();
                 while (dr.Read())
                 {
-                    yield return new Product { Name = dr["Name"].ToString() };
+                    yield return new Product
+                    {
+                        Name = dr["Name"].ToString(),
+                        Date = (DateTime)dr["ModifiedDate"]
+                    };
                 }
             }
         }
@@ -43,9 +49,12 @@ namespace SqlIntro
         {
             using (var conn = new MySqlConnection(_connectionString))
             {
+                conn.Open();
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = ""; //Write a delete statement that deletes by id
+                cmd.CommandText = "delete from product where ProductID = @id"; 
+                cmd.AddParam("@id", id);
                 cmd.ExecuteNonQuery();
+                
             }
         }
         /// <summary>
@@ -58,8 +67,9 @@ namespace SqlIntro
             //More on this in the future...  Nothing to do here..
             using (var conn = new MySqlConnection(_connectionString))
             {
+                conn.Open();
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = "update product set name = @name where id = @id";
+                cmd.CommandText = "update product set name = @name where ProductID = @id";
                 cmd.Parameters.AddWithValue("@name", prod.Name);
                 cmd.Parameters.AddWithValue("@id", prod.Id);
                 cmd.ExecuteNonQuery();
@@ -73,9 +83,28 @@ namespace SqlIntro
         {
             using (var conn = new MySqlConnection(_connectionString))
             {
+                conn.Open();
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = "INSERT into product (name) values(@name)";
+                cmd.CommandText = "INSERT into product (name, SellEndDate, ProductNumber, MakeFlag, " +
+                                  "FinishedGoodsFlag, Color, SafetyStockLevel, ReorderPoint, DaysToManufacture," +
+                                  "StandardCost, ModifiedDate, SellStartDate, ListPrice, RowGuid) values(@name, @SellEndDate, @ProductNumber, @MakeFlag," +
+                                  "@FinishedGoodsFlag, @Color, @SafetyStockLevel, @ReorderPoint, @DaysToManufacture, @StandardCost," +
+                                  "@ModifiedDate, @SellStartDate, @ListPrice, @RowGuid)";
                 cmd.Parameters.AddWithValue("@name", prod.Name);
+                cmd.Parameters.AddWithValue("@SellEndDate", prod.Date);
+                cmd.Parameters.AddWithValue("@ProductNumber", prod.ProductNumber);
+                cmd.Parameters.AddWithValue("@MakeFlag", prod.MakeFlag);
+                cmd.Parameters.AddWithValue("@FinishedGoodsFlag", prod.FinishedGoodsFlag);
+                cmd.Parameters.AddWithValue("@Color", prod.Color);
+                cmd.Parameters.AddWithValue("@SafetyStockLevel", prod.SafetyStockLevel);
+                cmd.Parameters.AddWithValue("@ReorderPoint", prod.ReorderPoint);
+                cmd.Parameters.AddWithValue("@DaysToManufacture", prod.DaysToManufacture);
+                cmd.Parameters.AddWithValue("@StandardCost", prod.StandardCost);
+                cmd.Parameters.AddWithValue("@ModifiedDate", prod.ModifiedDate);
+                cmd.Parameters.AddWithValue("@SellStartDate", prod.SellStartDate);
+                cmd.Parameters.AddWithValue("@ListPrice", prod.ListPrice);
+                cmd.Parameters.AddWithValue("@RowGuid", prod.ListPrice);
+
                 cmd.ExecuteNonQuery();
             }
         }
--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -6,14 +6,32 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = ""; //get connectionString format from connectionstrings.com and change to match your database
+            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=1234;"; //get connectionString format from connectionstrings.com and change to match your database
             var repo = new ProductRepository(connectionString);
-            foreach (var prod in repo.GetProducts())
-            {
-                Console.WriteLine("Product Name:" + prod.Name);
-            }
+            //foreach (var prod in repo.GetProducts())
+            //{
+            //    Console.WriteLine("Product Name: " + prod.Name + " Time: " + prod.Date.DayOfWeek);
+                
+            //}
 
-           
+            //repo.DeleteProduct(319);
+            repo.InsertProduct(new Product
+            {
+                Name = "Candace",
+                Color = "Red",
+                Date = new DateTime(1990,03,29),
+                ListPrice = 3.00,
+                ProductNumber = "2bornot2b",
+                SellStartDate = new DateTime(1990, 03, 29),
+                SafetyStockLevel = 3,
+                DaysToManufacture = 4,
+                FinishedGoodsFlag = true,
+                ModifiedDate = new DateTime(1990, 03, 29),
+                MakeFlag = false,
+                ReorderPoint = 77,
+                StandardCost = 5,
+            });
+            repo.UpdateProduct(new Product{Id = 1, Name = "Candace"});
             Console.ReadLine();
         }
 
